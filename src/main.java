efc10bf7173185fcbf34b69ed3b057c4cf1c18f8
Pavel <Test@mail.ru>
2023-02-23 21:09:06 +0400

    ChangeMain

--- src/main.java
+++ src/main.java
@@ -1,19 +1,19 @@
 public class main {
     public static void main(String[] args) {
-        int score = 100;
-        int amoun = 32176;
-        int bonus;
-        if (amoun >= 1000) {
-            bonus = amoun / 100;
+        int Score = 100;
+        int Amoun = 1100;
+        int Bonus;
+        if (Amoun > 1000) {
+            Bonus = Amoun / 100;
         } else {
-            bonus = 0;
+            Bonus = 0;
         }
-        int finishScore = score + amoun + bonus;
+        int finishScore = Score + Amoun + Bonus;
 
-        System.out.println("Пополнение счета составило " + amoun);
-        System.out.println("Бонусные баллы = " + bonus);
+        System.out.println("Пополнение счета составило " + Amoun);
+        System.out.println("Бонусные баллы = " + Bonus);
         System.out.println("Баланс = " + finishScore);
 
 
     }
-    }
+}
